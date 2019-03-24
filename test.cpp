a04ed44a0f89b9cf72f761a619934528ac93eccc
kelvinkristian <kelvinkristian4@gmail.com>
2019-03-24 16:52:26 +0700

    created polygon function

---
+++ test.cpp
@@ -0,0 +1,42 @@
+#include "GL/freeglut.h"
+#include "GL/gl.h"
+#include <math.h>
+
+/* Credits to Carmine at https://community.khronos.org/t/how-to-draw-a-pentagon/69776/3 */
+void renderFunction()
+{
+    int v;
+    float pent[5][2];
+
+    float ang, da = 6.2832 / 5.0;    // central angle between vertices in radians
+
+    // Compute vertex coordinates.
+
+    for (v = 0; v < 5; v++)  {
+        ang = v * da;
+        pent[v][0] = cos (ang);
+        pent[v][1] = sin (ang);
+    }
+
+    // Draw loop.
+    glClearColor(0.0, 0.0, 0.0, 0.0);
+    glClear(GL_COLOR_BUFFER_BIT);
+    glColor3f(0.5, 1.0, 0.0);
+    glOrtho(-1.0, 1.0, -1.0, 1.0, -1.0, 1.0);
+    glBegin (GL_POLYGON);
+    for (v = 0; v < 5; v++)  glVertex2fv(pent[v]);
+    glEnd();
+    glFlush();
+}
+
+int main(int argc, char** argv)
+{
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_SINGLE);
+    glutInitWindowSize(500,500);
+    glutInitWindowPosition(100,100);
+    glutCreateWindow("OpenGL - First window demo");
+    glutDisplayFunc(renderFunction);
+    glutMainLoop();    
+    return 0;
+}
